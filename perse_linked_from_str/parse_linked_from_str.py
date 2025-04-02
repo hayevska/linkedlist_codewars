e9f7bbecf83c6dd8fa8894aea26f934ce3b84da0
Yustyna Hayevska <justya200711@gmail.com>
2025-04-02 15:12:32 +0300

    Parse a linked list from a string

---
+++ perse_linked_from_str/parse_linked_from_str.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+    def __repr__(self):
+        return ' -> '.join(str(i) for i in self + ['None'])
+
+def linked_list_from_string(s):
+    if s == 'None':
+        curr = None
+    else:
+        s_lst = s.split(' -> ')[::-1]
+        for i in range(len(s_lst[:-1])):
+            if i == 0:
+                our_el = None
+            else:
+                our_el = curr
+            curr = Node(int(s_lst[i+1]))
+            curr.next = our_el
+        curr.data = int(s_lst[-1])
+    return curr
